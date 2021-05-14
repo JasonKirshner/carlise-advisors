884ee35347c0e99466da1a6de5846a67a92034fb
Jason Kirshner <jasonk@ruckusmarketing.com>
2021-05-13 22:10:38 -0400

    Adding homepage
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- wp-content/themes/carlise-advisors/templates/homepage.php
+++ wp-content/themes/carlise-advisors/templates/homepage.php
@@ -6,4 +6,13 @@
 
 get_header();
 
+the_module('hero');
+the_module('image-copy');
+the_module('copy-grid');
+the_module('image-copy');
+the_module('image-copy');
+the_module('image-copy');
+the_module('footer-cta');
+the_module('footer');
+
 get_footer();
