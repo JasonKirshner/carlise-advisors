64bb375270f5c23f1c4103e4a57db58e00ebb020
Jason Kirshner <jasonk@ruckusmarketing.com>
2021-05-13 20:36:16 -0400

    Adding 404
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

---
+++ wp-content/themes/carlise-advisors/modules/error-404/error-404.js
@@ -0,0 +1,10 @@
+/**
+* Initializes the site's error404 module.
+* @constructor
+* @param {Object} el - The site's error404 container element.
+*/
+const error404 = el => {
+  // console.log(el)
+}
+
+export default error404
---
+++ wp-content/themes/carlise-advisors/modules/error-404/error-404.php
@@ -0,0 +1,1 @@
+<section class="error-404<?= setted($class) ? " $class" : ""; ?>" data-module="error-404"></section>
