5b4547b31fc1672d7803fef922552bdb622ff5f8
unknown <dbriksza@gmail.com>
2021-05-19 20:24:47 -0400

    learning php

--- wp-content/themes/carlise-advisors/modules/hero/hero.php
+++ wp-content/themes/carlise-advisors/modules/hero/hero.php
@@ -1,4 +1,7 @@
 <section class="hero<?= setted($class) ? " $class" : ""; ?> placeholder" data-module="hero">
-  <?= $title ?>
+  <?= $hero_title ?>
+  <div class="container hero_image">
+    <?php the_module('image', array('image' => $hero_image)) ?>
+  </div>
   <h2 class="module-name">hero</h2>
 </section>
