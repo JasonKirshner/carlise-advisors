e90946e9630f197e059acb2b87b96869f6d1fdc7
dmoney410 <damanibasdeo@gmail.com>
2021-05-25 17:19:29 -0400

    404, need to fuck with custom fileds to get pic

--- wp-content/themes/carlise-advisors/modules/error-404/error-404.php
+++ wp-content/themes/carlise-advisors/modules/error-404/error-404.php
@@ -1,1 +1,16 @@
-<section class="error-404<?= setted($class) ? " $class" : ""; ?>" data-module="error-404"></section>
+<section class="error-404<?= setted($class) ? " $class" : ""; ?>" data-module="error-404">
+
+<table class='404-grid-table'>
+    <tr><td> <h1>4 0 4 </h1>
+    <h2> Son You Done Fucked Up </h2> </td>
+    <td> <div class='404-image'>
+    <?php the_module('image', array('image' => $four_image)) ?> 
+    </div> 
+    </td>
+    </tr>
+
+  </table>
+
+
+
+</section>
---
+++ wp-content/themes/carlise-advisors/templates/error404.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Template Name: Error 404
+ */
+get_header();
+
+the_module('error-404', array('four_image' => get_field('four_image')))
+
+get_footer();
