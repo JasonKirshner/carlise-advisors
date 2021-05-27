d5ba3b0e19bc58cfbf15d0d03758e990ebd9b6f7
dmoney410 <damanibasdeo@gmail.com>
2021-05-26 20:48:36 -0400

    404 edits

--- wp-content/themes/carlise-advisors/modules/error-404/error-404.php
+++ wp-content/themes/carlise-advisors/modules/error-404/error-404.php
@@ -1,8 +1,16 @@
 <section class="error-404<?= setted($class) ? " $class" : ""; ?>" data-module="error-404">
 
 <table class='404-grid-table'>
-    <tr><td> <h1>4 0 4 </h1>
-    <h2> Son You Done Fucked Up </h2> </td>
+    <tr><td class="error"> <h1>4 0 4 </h1>
+    <h3> Son You Done Fucked Up </h3> 
+    <div class="404-button"> <?php the_module('button', array(
+    'button' => $footer_button,
+    'use_button_tag' => true, // default false - optional
+    'link' => 'http://carlise-advisors.lndo.site/', // required if $tag == 'a'
+    'value' => 'Home', // required 
+    ));?> 
+    </div>
+    </td>
     <td> <div class='404-image'>
     <?php the_module('image', array('image' => $four_image)) ?> 
     </div> 
