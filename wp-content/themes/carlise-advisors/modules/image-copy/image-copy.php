1eb1ee20166b46591fe6ff9ac69e3f2f07d44e77
unknown <dbriksza@gmail.com>
2021-06-06 20:40:59 -0400

    added acf-json

--- wp-content/themes/carlise-advisors/modules/image-copy/image-copy.php
+++ wp-content/themes/carlise-advisors/modules/image-copy/image-copy.php
@@ -1,14 +1,14 @@
 <section class='image-copy<?= setted($class) ? ' $class' : ''; ?>' data-module='image-copy'>
-    <div class="image-copy-container">
-      <div class="image-copy__icon">
+    <div class='image-copy-container'>
+      <div class='image-copy__icon'>
         <?php the_module('image', array('image' => $icon)); ?>
       </div>
-        <h2><?php the_field($title) ?></h2>
-      <div class="image-copy-sub-container">
-        <div class="image-copy__image">
+        <h2><?= $title ?></h2>
+      <div class='image-copy-sub-container'>
+        <div class='image-copy__image'>
           <?php the_module('image', array('image' => $image)); ?>
         </div>
-        <div>
+        <div class='wysiwyg'>
           <?= $wysiwyg ?>
         </div>
       </div>
