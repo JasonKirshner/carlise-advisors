7ae035080848869b8bd3c2fc4d649e54a67042ad
hussainabutt <68175777+hussainabutt@users.noreply.github.com>
2021-06-06 20:52:56 -0400

    Navbar WIP

--- wp-content/themes/carlise-advisors/modules/navigation/navigation.js
+++ wp-content/themes/carlise-advisors/modules/navigation/navigation.js
@@ -2,8 +2,16 @@
  * Initializes the javascript for the navigation module
  * @param {Object} el - The DOM Node containing the data-module="navigation" attribute.
  */
+import scrollToElement from 'scroll-to-element'
 
 const navigation = (el) => {
+  const navLink = el.querySelectorAll('links')
+  navLink.forEach(element => {
+    element.addEventListener('click', () => {
+      const link = document.getElementById(element.getAttribute('data-link'))
+      scrollToElement(link, 'smooth')
+    })
+  })
   console.warn('initializing navigation module')
 }
 
--- wp-content/themes/carlise-advisors/modules/navigation/navigation.php
+++ wp-content/themes/carlise-advisors/modules/navigation/navigation.php
@@ -1,3 +1,16 @@
-<section class="navigation<?= setted($class) ? " $class" : ""; ?> placeholder" data-module="navigation">
-  <h2 class="module-name">navigation</h2>
+<section class="navigation" data-module="navigation">
+  <div class="container">
+    <div class="header_logo">
+      <?php
+      if (setted($header_logo)) :
+        the_module('image', array('image' => $header_logo));
+      endif;
+      ?>
+    </div>
+    <nav class='links'>
+      <button><?= $what_we_do['title'] ?></button>
+      <button><?= $about_us['title'] ?></button>
+      <button><?= $contact['title'] ?></button>
+    </nav>
+  </div>
 </section>
