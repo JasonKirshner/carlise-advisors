d902a3c3784e7dac3c9a16333eaf94efdfae37cb
Jason Kirshner <jasonk@ruckusmarketing.com>
2021-06-20 00:15:48 -0400

    Cleaning up nav
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- wp-content/themes/carlise-advisors/modules/navigation/navigation.js
+++ wp-content/themes/carlise-advisors/modules/navigation/navigation.js
@@ -6,16 +6,15 @@ import scrollToElement from 'scroll-to-element'
 
 const navigation = (el) => {
   const navLink = el.querySelectorAll('.link')
-  console.log(navLink)
+  const container = el.querySelector('.navigation__container')
+
   Array.from(navLink).forEach(element => {
     element.addEventListener('click', function () {
-      scrollToElement(this.value, { offset: -el.offsetHeight })
-      console.log(this.value)
-    }
-    )
+      scrollToElement(this.value, { offset: -container.offsetHeight })
+    })
   })
   const headerimage = el.querySelectorAll('.header_logo')
-  headerimage[0].addEventListener('click', function () { scrollToElement('.hero-container', { offset: -el.offsetHeight }) })
+  headerimage[0].addEventListener('click', function () { scrollToElement('.hero', { offset: -container.offsetHeight }) })
   console.warn('initializing navigation module')
 }
 
--- wp-content/themes/carlise-advisors/modules/navigation/navigation.php
+++ wp-content/themes/carlise-advisors/modules/navigation/navigation.php
@@ -1,20 +1,26 @@
 <section class="navigation z9" data-module="navigation">
   <div class="navigation__container z9">
     <div class="header_logo">
-      <?php
-      if (setted($header_logo)) :
-        the_module('image', array('image' => $header_logo, 'class' => 'image--relative'));
-      endif;
-      ?>
+      <?php if (is_404()) : ?>
+        <a href="<?= home_url(); ?>">
+        <?php endif; ?>
+        <?php if (setted($header_logo)) :
+          the_module('image', array('image' => $header_logo, 'class' => 'image--relative'));
+        endif; ?>
+        <?php if (is_404()) : ?>
+        </a>
+      <?php endif; ?>
     </div>
-    <nav class='links'>
-      <?php $field = get_field_object('nav_links');
-      while (have_rows('nav_links')) : the_row();
-      $value = get_sub_field('section');
-      $label = $field['sub_fields'][0]['choices'][$value];
-      ?>
-      <button class="link" value=".<?= $value ?>"><?= $label ?></button>
-      <?php endwhile?>
-    </nav>
+    <?php if (!is_404()) : ?>
+      <nav class='links'>
+        <?php $field = get_field_object('nav_links', 7);
+        while (have_rows('nav_links', 7)) : the_row();
+          $value = get_sub_field('section');
+          $label = $field['sub_fields'][0]['choices'][$value];
+        ?>
+          <button class="button-reset link p" value=".<?= $value ?>"><?= $label ?></button>
+        <?php endwhile ?>
+      </nav>
+    <?php endif; ?>
   </div>
 </section>
